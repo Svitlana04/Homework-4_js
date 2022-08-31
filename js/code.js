84ee190f4fa24228bf6d77d1f124b7994a21709f
Светлана Юхимович <svetik.iukhymovych@gmail.com>
2022-08-31 17:55:07 +0300

    code

--- js/code.js
+++ js/code.js
@@ -1,15 +1,18 @@
+// Задача 1
+function map(fn,arr){
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(fn(arr[i]));
+    }
+    return newArr;
+}
+ function fn(a) {return a*12.5;}
+ console.log(map(fn,[1,2,3,4,5]));
+ 
+// Задача 2
 function checkAge(){
     const age = prompt("Сколька вам лет");
     age > 18 || age >=18 ? true : confirm("Родители разрешили?");
 }
 checkAge();
 
-function map(fn,arr){
-    let newArr = [];
-    for(let i = 0; i < arr.length; i++){
-        newArr.push(fn(arr[i]));
-    }
-    return newArr;
-}
- function fn(x) {return x*12.5;}
- console.log(map(fn,[1,2,3,4,5]));
